c4bee26a8b6c67c5f0a64e9f3d264490fba5030d
Wayne-Okumu <145807991+Wayne-Okumu@users.noreply.github.com>
2024-01-02 11:58:53 +0300

    script.js

--- Wayne/js/script.js
+++ Wayne/js/script.js
@@ -9,13 +9,10 @@ loginForm.addEventListener('submit', (event) => {
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
 
-    // Simulate login request to a server
     if (email === 'kichwakubwa@gmail.com' && password === '12345six') {
-        // Successful login
         console.log('Login successful');
         window.location.href = '/dashboard.php';
     } else {
-        // Invalid credentials
         errorMessageElement.textContent = 'Invalid email or password';
         emailInput.value = '';
         passwordInput.value = '';
